96673bd5f172d6fccae9d0d3f28c3cfc35ce1fde
NeilMcConnellUbi <neil.mcconnell@ubisoft.com>
2019-10-25 00:34:15 -0400

    dead bees
    split the dead bee system into two jobified parts.

--- Original/CombatBees/Assets/Scripts/DeadBeeSystem.cs
+++ Original/CombatBees/Assets/Scripts/DeadBeeSystem.cs
@@ -8,70 +8,80 @@ using UnityEngine;
 using static Unity.Mathematics.math;
 
 [UpdateBefore(typeof(VelocityBasedMovement)), UpdateAfter(typeof(BeeBehaviour))]
-public class DeadBeeSystem : ComponentSystem
+[ExecuteAlways]
+public class DeadBeeSystem : JobComponentSystem
 {
+    EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+    Unity.Mathematics.Random rand;
 
-    protected override void OnUpdate()
+    struct ProcessDeadJob : IJobForEachWithEntity<Death, Velocity, BeeSize, FlightTarget, Translation>
+    {
+        public float deltaTime;
+        public float gravity;
+        public float floorY;
+        public NativeQueue<BloodInfo>.ParallelWriter BloodEffectsQueue;
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+        public Unity.Mathematics.Random rand;
+
+        public void Execute(Entity entity, int index, ref Death death, ref Velocity velocity, ref BeeSize beeSize, ref FlightTarget flightTarget, [ReadOnly] ref Translation translation)
+        {
+            if (flightTarget.entity != Entity.Null && flightTarget.isResource)
+            {
+                flightTarget.PendingAction = FlightTarget.Action.DropResource; //try to ensure we don't get floating entities
+            }
+
+            if (!death.FirstUpdateDone)
+            {
+                velocity.v *= 0.5f;
+                BloodEffectsQueue.Enqueue(new BloodInfo() { Position = translation.Value, Velocity = velocity.v, VelocityJitter = 2f, count = 6 });
+                death.FirstUpdateDone = true;
+                beeSize.Faded = true;
+            }
+
+            if (rand.NextFloat() < (death.DeathTimer - .5f) * .5f)
+            {
+                BloodEffectsQueue.Enqueue(new BloodInfo() { Position = translation.Value, Velocity = Vector3.zero, VelocityJitter = 6f, count = 1 });
+            }
+
+            velocity.v.y += gravity * deltaTime;
+
+            if (translation.Value.y <= floorY)
+            {
+                velocity.v = float3(0);
+            }
+
+            death.DeathTimer -= deltaTime;
+            if (death.DeathTimer < 0f)
+            {
+                CommandBuffer.DestroyEntity(index, entity);
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         float deltaTime = Time.fixedDeltaTime;
         NativeQueue<BloodInfo>.ParallelWriter BloodEffectsQueue = World.GetExistingSystem<StartFxSystem>().GetBloodQueue().AsParallelWriter();
 
-        EntityManager manager = World.Active.EntityManager;
-        Entities.ForEach((Entity e, ref PendingDeath p) =>
-        {
-            Entity target = p.EntityThatWillDie;
-            if (!manager.HasComponent<Death>(target) && manager.Exists(target))
-            {
-                manager.AddComponent<Death>(target);
-                manager.SetComponentData<Death>(target, new Death() { FirstUpdateDone = false, DeathTimer = 1 });
-            }
-        }
-        );
+        //the already dead (bees that have the Death tag)
+        var processDeadJob = new ProcessDeadJob();
+        processDeadJob.CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+        processDeadJob.BloodEffectsQueue = BloodEffectsQueue;
+        processDeadJob.deltaTime = deltaTime;
+        processDeadJob.gravity = Field.gravity;
+        processDeadJob.floorY = (-Field.size.y / 2);
+        processDeadJob.rand = rand;
+        rand.NextFloat();
+        JobHandle processDeadHandle = processDeadJob.Schedule(this, inputDeps);
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(processDeadHandle);
 
-        manager.DestroyEntity(GetEntityQuery(typeof(PendingDeath)));
+        return processDeadHandle;
+    }
 
-        using (NativeList<Entity> entitiesToDestroy = new NativeList<Entity>(Allocator.Temp))
-        {
-            Entities.ForEach((Entity e, ref Death death, ref Velocity velocity, ref Translation translation, ref FlightTarget flightTarget, ref BeeSize beeSize) =>
-            {
-                if (flightTarget.entity != Entity.Null && flightTarget.isResource) {
-                    flightTarget.PendingAction = FlightTarget.Action.DropResource;
-                }
-
-                if (!death.FirstUpdateDone)
-                {
-                    velocity.v *= 0.5f;
-                    //ParticleManager.SpawnParticle(translation.Value, ParticleType.Blood, velocity.v * .35f, 2f, 6);
-                    BloodEffectsQueue.Enqueue(new BloodInfo() { Position = translation.Value, Velocity = velocity.v, VelocityJitter = 2f, count = 6 });
-                    death.FirstUpdateDone = true;
-                    beeSize.Faded = true;
-                }
-
-                if (UnityEngine.Random.value < (death.DeathTimer - .5f) * .5f)
-                {
-                    //ParticleManager.SpawnParticle(translation.Value, ParticleType.Blood, Vector3.zero);
-                    BloodEffectsQueue.Enqueue(new BloodInfo() { Position = translation.Value, Velocity = Vector3.zero, VelocityJitter = 6f, count = 1 });
-                }
-
-                velocity.v.y += Field.gravity * deltaTime;
-
-                if (translation.Value.y < (-Field.size.y/2))
-                {
-                    velocity.v = float3(0);
-                }
-
-                death.DeathTimer -= deltaTime;
-
-                if (death.DeathTimer < 0f)
-                {
-                    entitiesToDestroy.Add(e);
-                }
-            });
-
-            foreach (Entity e in entitiesToDestroy)
-            {
-                manager.DestroyEntity(e);
-            }
-        }
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        rand = new Unity.Mathematics.Random(3);
     }
 }
